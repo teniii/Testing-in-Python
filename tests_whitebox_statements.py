f0de444a2f543e98f74a88a2fbbc622681c4415e
OMircea <oprea.mirceaioan@gmail.com>
2022-04-28 22:34:06 +0300

    Add statement tests

--- tests_whitebox_statements.py
+++ tests_whitebox_statements.py
@@ -1,10 +1,58 @@
 import unittest
 
-from problem import solve_problem
-
-# TODO: Add statement tests
+from problem import (
+    solve_problem,
+    OutOfBoundsException, WrongZooOrientationException
+)
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertNotEqual(True, False)
+    def test_valid_problem(self):
+        self.assertEqual(
+            solve_problem(
+                no_animals=5,
+                animals_coords=[(0, 2), (1, 1), (2, 2), (4, 5), (6, 7)],
+                no_zoos=3,
+                zoos_coords=[(0, 0, 1000, 1000),
+                             (-1000, -1000, 0, 0), (1, 0, 2, 2)]
+            ), [5, 0, 2])
+
+    def test_fail_bounds_animals(self):
+        with self.assertRaises(OutOfBoundsException):
+            solve_problem(
+                no_animals=7,
+                animals_coords=[(0, 0), (1, 0), (2, 0),
+                                (0, 1), (1, 1), (0, 2), (0, 3)],
+                no_zoos=2,
+                zoos_coords=[(0, 0, 1000, 1000),
+                             (-1000, -1000, 0, 0)]
+            )
+
+    def test_fail_bounds_zoos(self):
+        with self.assertRaises(OutOfBoundsException):
+            solve_problem(
+                no_animals=2,
+                animals_coords=[(0, 0), (1, 0), (2, 0),
+                                (0, 1), (1, 1), (0, 2), (0, 3)],
+                no_zoos=342,
+                zoos_coords=[(0, 0, 1000, 1000),
+                             (-1000, -1000, 0, 0)]
+            )
+
+    def test_wrong_zoo_orientation_1(self):
+        with self.assertRaises(WrongZooOrientationException):
+            solve_problem(
+                no_animals=2,
+                animals_coords=[(1, 14), (7, 2)],
+                no_zoos=2,
+                zoos_coords=[(-4, 10, 1, 2), (1, 2, 3, 4)]
+            )
+
+    def test_fail_bounds_coords(self):
+        with self.assertRaises(OutOfBoundsException):
+            solve_problem(
+                no_animals=2,
+                animals_coords=[(2, 15), (14, 14)],
+                no_zoos=2,
+                zoos_coords=[(-7863735754, 10, 1, 2), (1, 2, 3, 4)]
+            )
