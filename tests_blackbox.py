41a0b39047024721738047790451a0d50e536af7
OMircea <oprea.mirceaioan@gmail.com>
2022-04-29 21:33:42 +0300

    Change blackbox test class name and remove unnecessary test cases

--- tests_blackbox.py
+++ tests_blackbox.py
@@ -7,34 +7,7 @@ from problem import (
 )
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertNotEqual(True, False)
-
-    def test_init_animal(self):
-        animal = Animal(3, 5)
-        self.assertEqual(animal.x, 3)
-        self.assertEqual(animal.y, 5)
-
-    def test_init_zoo(self):
-        zoo = Zoo(0, 0, 100, 100, [])
-        self.assertEqual(zoo.x_start, 0)
-        self.assertEqual(zoo.y_start, 0)
-        self.assertEqual(zoo.x_end, 100)
-        self.assertEqual(zoo.y_end, 100)
-
-    def test_animals_inside(self):
-        animal = Animal(100, 5)
-        self.assertEqual(
-            animal.is_inside_fence(
-                fence_start_x=0,
-                fence_end_x=1000,
-                fence_start_y=0,
-                fence_end_y=1000), True)
-
-    # TODO for tests above: check if still needed
-    ###############################################################################
-
+class BlackboxTesting(unittest.TestCase):
     # Equivalence partitioning
     #
     # We've divided the problem into multiple classes, as follows:
